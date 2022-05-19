210739e76784896f12abd7d1c3dc3e007188ff2b
Bhargav Das Gupta <47706967+Hephaestus14089@users.noreply.github.com>
2022-05-19 23:16:50 +0530

    create download_wallpapers.sh
    a simple script to download pretty gentoo-based wallpapers

---
+++ download_wallpapers.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+wget "https://en.free-wallpapers.su/data/media/13/big/com0527.jpg"
+wget "https://en.free-wallpapers.su/data/media/13/big/com0565.jpg"
+wget "https://www.bwallpaperhd.com/wp-content/uploads/2018/08/GentooPenguinVideo.jpg"
+wget "https://cdn.wallpaperhub.app/cloudcache/7/6/d/9/8/9/76d9891a89eae8b123a478b0d1b194c7483bcdbd.jpg"
+wget "https://wallpapercave.com/wp/wp2491539.png"
+wget "https://wallpapercave.com/wp/wp2491551.jpg"
+wget "https://wallpapercave.com/wp/wp2491571.jpg"
+wget "https://images.hdqwalls.com/wallpapers/gentoo-linux-4k-ta.jpg"
